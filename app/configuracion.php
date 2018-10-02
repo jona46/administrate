e1ee1ed0f57efc12f779933791ef2d6e54aed75f
Jonatan Ostertag <jonatanostertag@gmail.com>
2018-10-01 23:43:46 -0300

    cio

--- app/configuracion.php
+++ app/configuracion.php
@@ -1,6 +1,6 @@
 <?php
- $host=""
- $name=""
- $username=""
- $pasword=""
+ $user_db="root";
+ $pass_db="";
+ $host="localhost";
+ $name_db="administrate";
 ?>
--- app/guarda_gasto.php
+++ app/guarda_gasto.php
@@ -1,9 +1,9 @@
 <?php
 include 'configuracion.php';
 
-
+$concepto = utf8_decode($_POST['concepto'])
 $valor = $_POST['valor_concepto'];
-$concepto = $_POST['concepto'];
+
 
 
 echo 'el concepto es '.$concepto.' y el valor es '.$valor;
